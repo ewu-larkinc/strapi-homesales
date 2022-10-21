991136420b2c8f7a785e4a3d1177e9cf4766c6d6
larkinchristopherj <larkin.christopherj@gmail.com>
2022-10-21 12:57:14 -0700

    initial project setup

---
+++ src/api/contact-family/controllers/contact-family.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-family controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::contact-family.contact-family');
